2a2b4c76cdb5eb633a33b02da58c9dad7d026cb9
Lauri Kinnunen <lkkinnunen@gmail.com>
2015-02-12 14:43:25 +0200

    removed unused dependencies

--- src/main/java/ui/MainFrame.java
+++ src/main/java/ui/MainFrame.java
@@ -2,9 +2,6 @@ package ui;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.JButton;
 import javax.swing.JFrame;
 
 /**
